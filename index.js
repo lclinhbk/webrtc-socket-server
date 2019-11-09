2120d6ba66d7b3a97967e54fa717b682b2859e69
lclinhbk <49783030+lclinhbk@users.noreply.github.com>
2019-11-09 13:02:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ io.on('connection', socket => {
         const isExist = arrUserInfo.some(e => e.ten === user.ten);
         socket.peerId = user.peerId;
         if (isExist) return socket.emit('DANG_KY_THAT_BAT');
-        arrUserInfo.push(user);
-        stt++;
+       
+        /*stt++;
         user.stt = stt;
         if (stt == 1) {
             firstCamId = user.peerId;
             firstCamTen = user.ten;
         }
         
-        user.firstCamId = firstCamId;
+        user.firstCamId = firstCamId;*/
+        
+        arrUserInfo.push(user);
         socket.emit('DANH_SACH_ONLINE', arrUserInfo);
         socket.broadcast.emit('CO_NGUOI_DUNG_MOI', user);
     });
@@ -26,13 +28,13 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
         const index = arrUserInfo.findIndex(user => user.peerId === socket.peerId);
         arrUserInfo.splice(index, 1);
-        if (stt > 0) {
+        /*if (stt > 0) {
              stt--;
         }
       
         if (user.peerId == firstCamId) {
             stt = 0;
-        }
+        }*/
         io.emit('AI_DO_NGAT_KET_NOI', socket.peerId);
     });
 });
